2c725c40b1d2ea16869a4496e8f53eb0f6e77a58
eCituk <ecituk@thefives.com.mx>
2023-07-24 18:22:51 -0500

    Detalles adicionales sobre funciones

--- src/cleanCode/03-functions.ts
+++ src/cleanCode/03-functions.ts
@@ -38,6 +38,20 @@
 
     }
 
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+
+        if ( isDead ) {
+            return 1500;
+        }
+        
+        if( isSeparated ){
+            return 2500;
+        }
+
+        return isRetired ? 3000 : 4000; 
+       
+    }
+
     
 
 
