24c0c61d97004ff30f78f247221ec70f97b18d74
eCituk <ecituk@thefives.com.mx>
2023-07-24 18:05:19 -0500

    Ejercicio con funciones

---
+++ src/cleanCode/03-functions.ts
@@ -0,0 +1,48 @@
+(() => {
+
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    function getMovieCastByCast( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+    
+
+    interface Movie {
+        cast:        string[];
+        description: string;
+        rating:      number;
+        title:       string;
+    }
+
+    function createMovie( {title, description, rating, cast } : Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
+
+
+
+
