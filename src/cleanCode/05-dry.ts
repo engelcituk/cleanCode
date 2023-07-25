b372dd734b257d977b6d75534517f5c7730f3d7f
eCituk <ecituk@thefives.com.mx>
2023-07-25 13:27:59 -0500

    Aplicando DRY

--- src/cleanCode/05-dry.ts
+++ src/cleanCode/05-dry.ts
@@ -11,11 +11,23 @@ class Product {
     ) {
         
     }
-
+    isProductReady() : boolean  {
+        for(const key in this){
+            switch ( typeof this[key] ) {
+                case 'string':
+                    if ( (<string>this[key]).length <= 0) throw Error(`${key} is empty`);
+                break;
+                case 'number':
+                    if ( (<number>this[key]) <= 0) throw Error(`${key} is zero`);
+                break;
+                default:
+                    throw Error(`${typeof this[key]} is not valid`);
+            }
+        }
+        return true
+    }
     toString(){
-        if (this.name.length === 0 ) throw Error('name is empty')
-        if (this.price <= 0 ) throw Error('price is zero')
-        if (this.size.length <= 0 ) throw Error('size is empty')
+        if( !this.isProductReady() ) return
         
         return `${this.name} (${this.price}), ${this.size}`
     }
