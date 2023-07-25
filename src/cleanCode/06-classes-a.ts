d7e9b6058419290378c4f1b947d8727af51bb68a
eCituk <ecituk@thefives.com.mx>
2023-07-25 16:14:48 -0500

    Herencia - Problemática

--- src/cleanCode/06-classes-a.ts
+++ src/cleanCode/06-classes-a.ts
@@ -9,12 +9,56 @@
                 public name: string,
                 public gender: Gender,
                 public birtdate : Date,
+            ) {}
+        }
+
+        class User extends Person {
+            
+            public lastAccess: Date;
+
+            constructor( 
+              public email: string,
+              public role: string,
+              name: string,
+              gender: Gender,
+              birthdate: Date,
             ) {
-        
+                super(name, gender, birthdate)
+                this.lastAccess = new Date()
+            }
+
+            checkCredentials(){
+                return true
             }
         }
 
-        const newPerson = new Person('eCituk', 'M', new Date('1991-11-15') )
-        console.log(newPerson)
+        class UserSettings extends User {
+            constructor(
+                public workingDirectory: string,
+                public lastOpenFolder  : string,
+                email                  : string,
+                role                   : string,
+                name                   : string,
+                gender                 : Gender,
+                birthdate              : Date,
+
+            ){
+                super(email, role, name, gender, birthdate)
+            }
+        }
+        
+        const userSettings = new UserSettings(
+            '/user/home',
+            '/user/imgs',
+            'myemail@mail.com',
+            'Admin',
+            'eCituk',
+            'M',
+            new Date('1991-11-15')
+        )
+
+        console.log({userSettings})
+
+        
     }
 )()
