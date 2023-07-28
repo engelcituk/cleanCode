082b07030732178adb6c198db6434ae839bd8c5c
eCituk <ecituk@thefives.com.mx>
2023-07-28 15:40:50 -0500

    Ejercicio - Substitución de Liskov

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import './style.css'
 // import './cleanCode/07-tarea'
 // import './code-smells/02-hight-coupling'
 import './solid/02-open-close-a'
+import './solid/03-liskov-a'
+
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
