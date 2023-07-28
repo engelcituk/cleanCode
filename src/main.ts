02ebbc16865599484bd76b84b92545f69e998777
eCituk <ecituk@thefives.com.mx>
2023-07-28 17:38:33 -0500

    Ejercicio de inversión de dependencias

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css'
 // import './cleanCode/07-tarea'
 // import './code-smells/02-hight-coupling'
 // import './solid/02-open-close-a'
-import './solid/03-liskov-a'
+// import './solid/03-liskov-a'
+import './solid/05-dependency-a'
 
 
 
