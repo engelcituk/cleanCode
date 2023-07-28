0eee0ad4ed8dc7ad8b617aee983c3592d5a41e16
eCituk <ecituk@thefives.com.mx>
2023-07-28 15:14:23 -0500

    Remover la dependencia de Axios

--- src/solid/03-open-close-c.ts
+++ src/solid/03-open-close-c.ts
@@ -2,9 +2,16 @@ import axios from "axios";
 
 export class HttpClient {
 
-    async get( url: string ) {
+    /*async get( url: string ) {
         const { data, status } = await axios.get( url )
         console.log({status})
         return {status, data}
+    }*/
+
+    async get(url: string){
+        const resp = await fetch(url)
+        const data = await resp.json()
+
+        return {data, status: resp.status}
     }
 }
