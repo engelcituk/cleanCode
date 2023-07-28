173ee7e3bff66845d36ae84c8b78fd37b188bbc1
eCituk <ecituk@thefives.com.mx>
2023-07-28 12:24:01 -0500

    Ejercicio de OCP

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './style.css'
 // import './cleanCode/06-classes-c'
 // import './cleanCode/07-tarea'
 // import './code-smells/02-hight-coupling'
-import './solid/01-srp'
+import './solid/02-open-close-a'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
