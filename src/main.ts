b0ad8e04ff32ae089e48737633f287c1a2c88ad9
eCituk <ecituk@thefives.com.mx>
2023-07-28 11:15:26 -0500

    Ejemplo de SRP

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 // import './cleanCode/05-dry'
 // import './cleanCode/06-classes-c'
 // import './cleanCode/07-tarea'
-import './code-smells/02-hight-coupling'
+// import './code-smells/02-hight-coupling'
+import './solid/01-srp'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
